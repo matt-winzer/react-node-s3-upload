d99dddc336fab68efad414ab6cc398ff2d343037
Matt Winzer <matt.winzer@gmail.com>
2020-07-13 13:58:15 -0600

    Serve client on / route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.port || 3000;
 
+// middleware
 app.use(morgan('dev'));
+
+// routes
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
